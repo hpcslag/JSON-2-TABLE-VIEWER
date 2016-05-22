61ce2ddc5c9f3b1c9760cef575c834ee6ee058a6
MacTaylor <hpcslag@users.noreply.github.com>
2016-05-22 10:56:57 +0800

    Filename extension modify error fixed

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ if(!!filepath){
 			html += ("<script src=\"https://code.jquery.com/jquery-2.2.3.min.js\" integrity=\"sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=\" crossorigin=\"anonymous\"></script><script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\" integrity=\"sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS\" crossorigin=\"anonymous\"></script>");
 			html += ("</body></html>");
 
-			var ws = fs.createWriteStream(filepath.split(".")[filepath.split(".").length - 1] + ".html");
+			var NewFileName = filepath.substr(0, filepath.lastIndexOf(".")) + ".html";
+			var ws = fs.createWriteStream(NewFileName);
 			ws.write(html);
 			ws.end();
 		}
